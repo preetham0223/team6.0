7646c78d9aff7c7b711edcd929114541d245b165
preetham0223 <sagilinoyal@gmail.com>
2024-07-11 12:37:16 +0530

    team6

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Team team1 = new Team();
+        team1.name();
+
+    }
+}
---
+++ src/Team.java
@@ -0,0 +1,8 @@
+public class Team {
+    public void name(){
+        System.out.println("nayeem");
+        System.out.println("astik");
+        System.out.println("preetham");
+        
+    }
+}
